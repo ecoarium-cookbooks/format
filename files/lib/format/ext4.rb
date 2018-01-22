f2865479657ab75169942dfff9ffd9803f22e80d
Jay Flowers <jay.flowers@gmail.com>
2018-01-22 10:24:02 -0500

    commit for version : 1.0.2

--- files/lib/format/ext4.rb
+++ files/lib/format/ext4.rb
@@ -7,10 +7,13 @@ module Format
       shell_command! "parted #{device} mklabel gpt" unless shell_true?("parted #{device} print | grep gpt")
       shell_command! "yes | parted #{device} mkpart logical ext4 1 -- \"-1\"" unless shell_true?("parted #{device} print | grep logical | grep -v physical")
       shell_command! "parted #{device} set 1 lvm on" unless shell_true?("parted #{device} print | grep lvm")
-      shell_command! "mkfs.ext4 #{device}" , {:timeout => timeout} 
+      shell_command! "mkfs.ext4 #{device}" , {:timeout => timeout}
     end
 
     def should_run?
+      pkg_install = Chef::Resource::Package.new("parted", run_context)
+      pkg_install.run_action(:install)
+
       !shell_true?("parted #{device} print | grep ext4")
     end
   end
